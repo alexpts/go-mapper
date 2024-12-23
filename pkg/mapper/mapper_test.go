8058aef1e83cd81c9426e6a3281ab436a5f6dc53
alexpts <alexpts@yandex.ru>
2024-12-23 10:58:37 +0300

    refactoring, add case for read private fields

--- pkg/mapper/mapper_test.go
+++ pkg/mapper/mapper_test.go
@@ -9,10 +9,16 @@ import (
 	"github.com/alexpts/go-mapper/pkg/model"
 )
 
-func TestSimpleConvertModelToDTO(t *testing.T) {
-	mapper := Mapper[model.UserModel, model.UserDto]{
+func createMapper[Src any, Dest any]() Mapper[Src, Dest] {
+	return Mapper[Src, Dest]{
 		TypeManager: NewReflectManager(),
 	}
+}
+
+// TestSimpleConvertModelToDTO
+// Все публичные поля зеркалируются в target модель
+func TestSimpleConvertModelToDTO(t *testing.T) {
+	mapper := createMapper[model.UserModel, model.UserDto]()
 
 	in := model.NewUserModel("alex", 12)
 	actual, err := mapper.Convert(in)
@@ -21,5 +27,49 @@ func TestSimpleConvertModelToDTO(t *testing.T) {
 	expected := model.UserDto{
 		Age: 12, Name: "alex",
 	}
+	assert.Equal(t, expected, actual)
+}
+
+// TestPartField
+// Проверяем, что по дефолту приватные поля в target моделе не заполняются (пропускаются)
+func TestPartField(t *testing.T) {
+	type DTO struct {
+		age  int
+		Name string
+	}
+
+	mapper := createMapper[model.UserModel, DTO]()
+
+	actual, err := mapper.Convert(
+		model.NewUserModel("alex", 12),
+	)
+
+	require.NoError(t, err)
+
+	// Default skip private target field
+	expected := DTO{age: 0, Name: "alex"}
+	assert.Equal(t, expected, actual)
+}
+
+// TestReadPrivateProperty
+// Проверям чтение приватного поля из ихсодной модели
+func TestReadPrivateProperty(t *testing.T) {
+	type Model struct {
+		Age  int
+		name string
+	}
+
+	type DTO struct {
+		Age  int
+		Name string
+	}
+
+	mapper := createMapper[Model, DTO]()
+
+	actual, err := mapper.Convert(&Model{name: "alex", Age: 12})
+
+	require.NoError(t, err)
+
+	expected := DTO{Age: 12, Name: "alex"}
 	assert.Equal(t, expected, actual)
 }
